abb356493999ed83fa862f6637defc5db1469558
João Vitor Marques <Joo Vitor Marques>
2017-05-30 00:51:59 -0300

    start no json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./config/express')();
 
-var port = 3000
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log("Servidor rodando na porta: "+port);
 });
