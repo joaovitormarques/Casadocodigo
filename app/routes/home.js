187f68d4bb53a6515f64e1541627bd1ec6aebf9f
João Vitor Marques <Joo Vitor Marques>
2017-06-12 11:13:07 -0300

    ace não funciona ainda

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,34 @@
 module.exports = function(app) {
-    app.get("/",function(req, res) {
+	app.get("/",function(req, res) {
 
-            res.render('home/index');
+		res.render('home/index');
 
-    });
+	});
+
+
+	
+
+
+app.post('/home', function(req, res){
+	var codigo = req.body.codigo;
+	var validadorCodigo = req.assert('codigo', 'Titulo deve ser preenchido');
+	validadorCodigo.notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.format({
+			html: function(){
+				res.status(400).render("/");
+			},
+			json: function(){
+				res.status(400).send(errors);
+			}
+		});
+		return;
+	}
+	console.log('Enviou o seguinte código: '+ codigo);
+	eval(codigo);
+	res.render('produtos/lista');
+});
 }
